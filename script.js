eb9a9367422cd2f691d98b2e8cb6d49a74c37b44
Sreekar <ksreekar407@gmail.com>
2021-11-29 19:54:15 +0530

    Reset Button Working

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-function tipAmountPerPerson (bill, tipPercent, people) {
-    return (bill*tipPercent)/people
+function myFunction() {
+    document.getElementById("frm").reset();
 }
-function totalPerPerson (bill, tipPercent, people) {
-    return (bill + (bill*tipPercent))/people
-}
-
-console.log(tipAmountPerPerson(142.55, 0.15, 5))
-console.log(totalPerPerson(142.55, 0.15, 5))
